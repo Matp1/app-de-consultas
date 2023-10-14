4a2924cfcb8f1fe7a2ff493ffcbe197766040bc3
JLucian07 <146896514+JLucian07@users.noreply.github.com>
2023-10-14 17:20:49 -0300

    Delete Site directory

--- Site/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("paciente-card").addEventListener("click", function() {
-    window.location.href = "login.php"; 
-  });
-
-  document.getElementById("medico-card").addEventListener("click", function() {
-    window.location.href = "login.php"; 
-  });
