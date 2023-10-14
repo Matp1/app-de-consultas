70535bd32877e4e283d5641fe4cbf8670ce4024e
Gustavo <127802174+gugisss@users.noreply.github.com>
2023-10-13 21:45:46 -0300

    Update cadastro.php

--- site/cadastro.php
+++ site/cadastro.php
@@ -1,24 +1,46 @@
 <?php
-    require_once "config.php";
+require_once "config.php";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST"){
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+$response = array();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Verificar se o email já está cadastrado
+    $checkEmailSQL = "SELECT id FROM users WHERE email = ?";
+    $checkEmailStmt = $conn->prepare($checkEmailSQL);
+    $checkEmailStmt->bind_param("s", $email);
+    $checkEmailStmt->execute();
+    $checkEmailStmt->store_result();
+
+    if ($checkEmailStmt->num_rows > 0) {
+        $response['success'] = false;
+        $response['message'] = "Este email já está cadastrado.";
+    } else {
+        // Email não encontrado, prosseguir com o cadastro
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-        $sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
-        $stmt = $conn->prepare($sql);
+        $insertSQL = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+        $stmt = $conn->prepare($insertSQL);
         $stmt->bind_param("sss", $name, $email, $hashed_password);
 
-        if ($stmt->execute()){
-            echo "Usuário criado com sucesso";
-        }else{
-            echo "Erro: " . $sql . "<br>" . $conn->error;
+        if ($stmt->execute()) {
+            $response['success'] = true;
+            $response['message'] = "Cadastro efetuado com sucesso!";
+        } else {
+            $response['success'] = false;
+            $response['message'] = "Erro: " . $insertSQL . "<br>" . $conn->error;
         }
-
         $stmt->close();
     }
 
-    $conn->close();
+    $checkEmailStmt->close();
+}
+
+$conn->close();
+
+header('Content-Type: application/json');
+echo json_encode($response);
 ?>
