795380fa75be2bf1842048bc4f28c349739abc82
JLucian07 <146896514+JLucian07@users.noreply.github.com>
2023-10-14 16:14:33 -0300

    Add files via upload

---
+++ Site/script.js
@@ -0,0 +1,7 @@
+document.getElementById("paciente-card").addEventListener("click", function() {
+    window.location.href = "login.php"; 
+  });
+
+  document.getElementById("medico-card").addEventListener("click", function() {
+    window.location.href = "login.php"; 
+  });
