bb4d116ff70798f990390171ccf70e6731088d0b
Gustavo <127802174+gugisss@users.noreply.github.com>
2023-10-13 21:46:25 -0300

    Update config.php

--- site/config.php
+++ site/config.php
@@ -5,7 +5,7 @@
 
     $servername = "localhost";
     $username = "root";
-    $password = "projetoterca";
+    $password = "";
     $dbname = "login";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
