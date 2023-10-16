aa1d725b505ad71c6af7cb323e8a350a62ad289e
Chiquin <99442194+leoziN77@users.noreply.github.com>
2023-10-16 11:10:35 -0300

    Add files via upload

---
+++ site/dump.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE login;
+
+USE login;
+
+CREATE TABLE users (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    first_visit TINYINT(1) NOT NULL DEFAULT 1
+);
+
+-- ALTER TABLE users
+-- ADD COLUMN first_visit INT(1) DEFAULT 1 NOT NULL;
